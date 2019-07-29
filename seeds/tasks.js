30c40c5c3f35444657d3129f56a979211f78753c
Jherod Brendon <jherodjb@gmail.com>
2019-07-29 11:09:28 -0300

    Concertando o troço

--- seeds/tasks.js
+++ seeds/tasks.js
@@ -1,8 +1,6 @@
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
   return knex("tasks").del()
     .then(function () {
-      // Inserts seed entries
       return knex("tasks").insert([
         {title: "Bom dia", description: "Você deu bom dia para alguém há 44 dias"},
         {title: "Ligação", description: "Você ligou para seus amigos há 44 dias"},
